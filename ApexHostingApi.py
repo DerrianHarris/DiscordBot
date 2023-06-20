0a218533981fd509e803c1dcff76f89a76845cc4
Derrian Harris <dsh0018au@gmail.com>
2023-06-20 01:49:01 -0500

    Initial commit

---
+++ ApexHostingApi.py
@@ -0,0 +1,208 @@
+import os
+import random
+import time
+import re
+
+import undetected_chromedriver as uc
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+load_dotenv()
+
+
+class ApexHostingApi:
+    APH_USERNAME = os.getenv("APH_USERNAME")
+    APH_PASSWORD = os.getenv("APH_PASSWORD")
+    ApexHostingPanelLoginURL = "https://panel.apexminecrafthosting.com/"
+    ApexHostingPanelServerDashboardURL = "https://panel.apexminecrafthosting.com/server/"
+    ServerID = None
+
+    def __init__(self, headless=True, min_timeout=5, max_timeout=10):
+        options = uc.ChromeOptions()
+        options.add_argument(
+            "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/80.0.3987.163 Safari/537.36")
+        if headless:
+            options.add_argument('--headless')
+        self.driver = uc.Chrome(options=options)
+        self.min_timeout = min_timeout
+        self.max_timeout = max_timeout
+
+    def get_server_status(self):
+        try:
+            print("Getting Server Status")
+            soup = self.go_to_console()
+            status_icon_div = soup.find('div', id='statusicon-ajax')
+            img_source = status_icon_div.img.attrs['src']
+            status = img_source.split("/")[-1].split(".png")[0]
+            return status
+        except Exception as err:
+            print(err)
+            raise
+
+    def stop_server(self):
+        try:
+            print("Stopping Server")
+            self.go_to_console()
+            button = self.driver.find_element("name", "yt1")
+            if button.get_attribute('disabled') is not None:
+                raise Exception("Failed to stop server.")
+            button.click()
+            print("Command sent!")
+        except Exception as err:
+            print(err)
+            raise
+
+    def force_stop_server(self):
+        try:
+            print("Force stopping Server")
+            self.go_to_console()
+            button = self.driver.find_element("name", "yt3")
+            if button.get_attribute('disabled') is not None:
+                raise Exception("Failed to force stop server.")
+            button.click()
+            print("Command sent!")
+        except Exception as err:
+            print(err)
+            raise
+
+    def start_server(self):
+        try:
+            print("Starting Server")
+            self.go_to_console()
+            button = self.driver.find_element("name", "yt0")
+            if button.get_attribute('disabled') is not None:
+                raise Exception("Failed to start server.")
+            button.click()
+            print("Command sent!")
+        except Exception as err:
+            print(err)
+            raise
+
+    def restart_server(self):
+        try:
+            print("Restarting Server")
+            self.go_to_console()
+            button = self.driver.find_element("name", "yt2")
+            if button.get_attribute('disabled') is not None:
+                raise Exception("Failed to restart server.")
+            button.click()
+            print("Command sent!")
+        except Exception as err:
+            print(err)
+            raise
+
+    def go_to_dashboard(self):
+        self.driver.get(self.get_server_dashboard_url())
+        time.sleep(self.get_timeout())
+        page_source = self.driver.page_source
+        soup = BeautifulSoup(page_source, features="html.parser")
+        if soup.find('div', id='statusicon-ajax') is None:
+            raise Exception("Server dashboard failed to load.")
+        return soup
+
+    def go_to_console(self):
+        self.driver.get(self.get_server_console_url())
+        time.sleep(self.get_timeout())
+        page_source = self.driver.page_source
+        soup = BeautifulSoup(page_source, features="html.parser")
+        if soup.find('div', id='statusicon-ajax') is None:
+            raise Exception("Server dashboard failed to load.")
+        return soup
+
+    def run_console_command(self, command):
+        try:
+            print("Running console command: ", command)
+            self.go_to_console()
+            self.driver.find_element("id", "command").send_keys(command)
+            button = self.driver.find_element("name", "yt4")
+            if button.get_attribute('disabled') is not None:
+                raise Exception("Failed to send command to server.")
+            button.click()
+            print("Command sent!")
+        except Exception as err:
+            print(err)
+            raise
+
+    def get_console_log(self, lines=10):
+        try:
+            print("Getting console logs")
+            soup = self.go_to_console()
+            console_log = soup.find('div', id='log-ajax')
+            log_entry_regex = "\d{2}.\d{2} \d{2}:\d{2}:\d{2}"
+            entries = re.sub(log_entry_regex, lambda x: '\n' + x.group(0), console_log.text)
+            entries = entries.split('\n')
+            return list(filter(None, entries))[-lines:]
+        except Exception as err:
+            print(err)
+            raise
+
+    def get_server_dashboard_url(self):
+        return self.ApexHostingPanelServerDashboardURL + self.ServerID
+
+    def get_server_console_url(self):
+        return self.ApexHostingPanelServerDashboardURL + "log/" + self.ServerID
+
+    def login(self):
+        try:
+            print("Loading Login Page")
+            self.driver.get(self.ApexHostingPanelLoginURL)
+            element = None
+            try:
+                element = WebDriverWait(self.driver, self.get_timeout()).until(
+                    EC.presence_of_element_located((By.ID, 'LoginForm_name')))
+            except:
+                pass
+            if element is None:
+                page_source = self.driver.page_source
+                soup = BeautifulSoup(page_source, features="html.parser")
+                if soup.find('li', id='logout_link') is not None:
+                    # We are already logged in
+                    print("Already Logged In! Skipping...")
+                    self.get_server_id()
+                    return
+                else:
+                    raise Exception("Login Page Unable to load")
+            print("Login Page Loaded")
+            self.driver.find_element("id", "LoginForm_name").send_keys(self.APH_USERNAME)
+            self.driver.find_element("id", "LoginForm_password").send_keys(self.APH_PASSWORD)
+            self.driver.find_element("name", "yt0").click()
+            print("Trying login...")
+            time.sleep(self.get_timeout())
+            page_source = self.driver.page_source
+            soup = BeautifulSoup(page_source, features="html.parser")
+            soup.find('div', id='LoginForm_name')
+            if soup.find('div', class_='errorMessage') is not None:
+                raise Exception("Login Failed. Either Username/Password is incorrect")
+            if soup.find('li', id='logout_link') is None:
+                raise Exception("Login Failed. Api blocked by Url.")
+            print("Login Succeeded")
+            self.get_server_id()
+        except Exception as err:
+            print(err)
+            raise
+
+    def get_server_id(self):
+        page_source = self.driver.page_source
+        soup = BeautifulSoup(page_source, features="html.parser")
+        server_index = soup.find('a', class_='btn btn-primary btn-block').attrs['href']
+        server_index_url = self.ApexHostingPanelLoginURL + server_index[1:]
+        self.driver.get(server_index_url)
+        element = None
+        try:
+            element = WebDriverWait(self.driver, self.get_timeout()).until(
+                EC.presence_of_element_located((By.ID, 'statusicon-ajax')))
+        except:
+            pass
+        if element is None:
+            raise Exception("Server dashboard failed to load.")
+        url = self.driver.current_url
+        self.ServerID = url.split('/')[-1]
+        print("Server Id: ", self.ServerID)
+        return self.ServerID
+
+    def get_timeout(self):
+        return random.randint(self.min_timeout, self.max_timeout)
---
+++ main.py
@@ -0,0 +1,201 @@
+import os
+import logging
+import discord
+from typing import Optional
+from dotenv import load_dotenv
+from discord import app_commands
+from ApexHostingApi import ApexHostingApi
+
+load_dotenv()
+
+MY_GUILD = discord.Object(id=1057166212536217680)
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+aph=ApexHostingApi()
+logging.basicConfig(format='%(asctime)s: [%(levelname)s] %(message)s',level=logging.DEBUG, handlers=[
+        logging.FileHandler("discord.log"),
+        logging.StreamHandler()
+    ])
+
+role_name="Admin"
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+
+@client.event
+async def on_ready():
+    logging.info(f'Logged in as {client.user} (ID: {client.user.id})')
+    logging.info('------')
+
+
+@client.tree.command()
+async def get_server_status(interaction: discord.Interaction):
+    """Gets Current Server Status"""
+    await interaction.response.defer()
+    log_requests(interaction, f'get_server_status')
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+    user = interaction.user
+
+    if role not in interaction.user.roles:
+        await interaction.followup.send(f"{user.name} does not have the correct role! Role: {role_name}")
+        return
+
+    Status_Message = "Sorry, I could not load the server status. :("
+    try:
+        aph.login()
+        Status_Message = f'```Current server status: {aph.get_server_status()}```'
+    except Exception as err:
+        logging.error(err)
+    await interaction.followup.send(Status_Message)
+
+@client.tree.command()
+async def start_server(interaction: discord.Interaction):
+    """Starts the server"""
+    await interaction.response.defer()
+    log_requests(interaction, f'start_server')
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+    user = interaction.user
+    if role not in interaction.user.roles:
+        await interaction.followup.send(f"{user.name} does not have the correct role! Role: {role_name}")
+        return
+
+    command_msg = "Sorry, I could not start the server. :("
+    try:
+        aph.login()
+        aph.start_server()
+        command_msg = f'```Command Sent Successfully!```'
+    except Exception as err:
+        logging.error(err)
+    await interaction.followup.send(command_msg)
+
+@client.tree.command()
+async def stop_server(interaction: discord.Interaction):
+    """Stops the server"""
+    await interaction.response.defer()
+    log_requests(interaction, f'stop_server')
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+    user = interaction.user
+    if role not in interaction.user.roles:
+        await interaction.followup.send(f"{user.name} does not have the correct role! Role: {role_name}")
+        return
+
+    command_msg = "Sorry, I could not stop the server. :("
+    try:
+        aph.login()
+        aph.stop_server()
+        command_msg = f'```Command Sent Successfully!```'
+    except Exception as err:
+        logging.error(err)
+    await interaction.followup.send(command_msg)
+
+@client.tree.command()
+async def restart_server(interaction: discord.Interaction):
+    """Restarts the server"""
+    await interaction.response.defer()
+    log_requests(interaction, f'restart_server')
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+    user = interaction.user
+    if role not in interaction.user.roles:
+        await interaction.followup.send(f"{user.name} does not have the correct role! Role: {role_name}")
+        return
+
+    command_msg = "Sorry, I could not restart the server. :("
+    try:
+        aph.login()
+        aph.restart_server()
+        command_msg = f'```Command Sent Successfully!```'
+    except Exception as err:
+        logging.error(err)
+    await interaction.followup.send(command_msg)
+
+@client.tree.command()
+async def force_stop_server(interaction: discord.Interaction):
+    """Force stops the server"""
+    await interaction.response.defer()
+    log_requests(interaction, f'force_stop_server')
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+    user = interaction.user
+    if role not in interaction.user.roles:
+        await interaction.followup.send(f"{user.name} does not have the correct role! Role: {role_name}")
+        return
+
+    command_msg = "Sorry, I could not force stop the server. :("
+    try:
+        aph.login()
+        aph.force_stop_server()
+        command_msg = f'```Command Sent Successfully!```'
+    except Exception as err:
+        logging.error(err)
+    await interaction.followup.send(command_msg)
+
+@client.tree.command()
+@app_commands.describe(
+    command='Command you want to run on the console',
+)
+async def run_console_command(interaction: discord.Interaction, command: str):
+    """Run a command using the server console log"""
+    await interaction.response.defer()
+    log_requests(interaction, f'run_console_command [command={command}]')
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+    user = interaction.user
+    if role not in interaction.user.roles:
+        await interaction.followup.send(f"{user.name} does not have the correct role! Role: {role_name}")
+        return
+
+    command_msg = "Sorry, I could not send the command. :("
+    try:
+        aph.login()
+        aph.run_console_command(command)
+        command_msg = f'```Command Sent Successfully!```'
+    except Exception as err:
+        logging.error(err)
+    await interaction.followup.send(command_msg)
+
+# To make an argument optional, you can either give it a supported default argument
+# or you can mark it as Optional from the typing standard library. This example does both.
+@client.tree.command()
+@app_commands.describe(lines='The number of lines you want from the logs. Min: 1 Max: 20 Default: 10')
+async def get_console_log(interaction: discord.Interaction, lines: Optional[app_commands.Range[int, 1, 20]] = 10):
+    """Returns last messages from console logs. Defaults to 10"""
+    await interaction.response.defer()
+    log_requests(interaction,f'get_console_log [lines={lines}]')
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+    user = interaction.user
+    if role not in interaction.user.roles:
+        await interaction.followup.send(f"{user.name} does not have the correct role! Role: {role_name}")
+        return
+
+    Log_Message = "Sorry, I could not load the console log. :("
+    try:
+        aph.login()
+        console_log = aph.get_console_log(lines)
+        console_log = '\n'.join(console_log)
+        Log_Message = f'## Console Log\nLast {lines} lines```{console_log}```'
+    except Exception as err:
+        logging.error(err)
+    await interaction.followup.send(Log_Message)
+
+def log_requests(interaction: discord.Interaction,command: str):
+    user = interaction.user
+    logging.debug(f'Username: {user.name} | Roles: {user.roles} | Request: {command}')
+
+client.run(os.getenv('DISCORD_TOKEN'))
