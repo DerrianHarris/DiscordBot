9ddd151925e394ccfbf0ae8fa86561692c014d62
Derrian Harris <dsh0018au@gmail.com>
2024-04-08 20:59:52 -0500

    Nitrado api setup

---
+++ NitradoApi.py
@@ -0,0 +1,98 @@
+import os
+import random
+import time
+import re
+import requests
+from dotenv import load_dotenv
+from rcon.source import Client
+
+load_dotenv()
+
+class NitradoApi:
+    def __init__(self):
+        self.NITRADO_TOKEN = os.getenv("NITRADO_TOKEN")
+        self.SERVER_ID = os.getenv("SERVER_ID")
+        self.RCON_IP_FULL = os.getenv("RCON_IP_FULL")
+        split = self.RCON_IP_FULL.split(":")
+        self.RCON_PWD = os.getenv("RCON_PWD")
+        self.RCON_IP = split[0]
+        self.RCON_PORT = int(split[1])
+        pass
+    async def get_server_status(self):
+        print("Getting Server Status")
+        response = requests.get(f'https://api.nitrado.net/services/{self.SERVER_ID}/gameservers', auth=BearerAuth(self.NITRADO_TOKEN))
+        print("Status Code: " + str(response.status_code))
+        print("Response: " + str(response.content))
+        if response.status_code == 200:
+            response_json = response.json()
+            status = response_json["data"]["gameserver"]["status"]
+            return status
+        return None
+    async def stop_server(self):
+        print("Stopping Server")
+        response = requests.post(f'https://api.nitrado.net/services/{self.SERVER_ID}/gameservers/stop',
+                                auth=BearerAuth(self.NITRADO_TOKEN))
+        print("Status Code: " + str(response.status_code))
+        print("Response: " + str(response.content))
+        if response.status_code == 200:
+            response_json = response.json()
+            status = response_json["message"]
+            return status
+        return None
+
+    async def start_server(self,game="arksa"):
+        print("Starting Server for " + game)
+        payload = {'game': game}
+        response = requests.post(f'https://api.nitrado.net/services/{self.SERVER_ID}/gameservers/games/start',
+                                 auth=BearerAuth(self.NITRADO_TOKEN), params=payload)
+        print("Status Code: " + str(response.status_code))
+        print("Response: " + str(response.content))
+        if response.status_code == 200:
+            response_json = response.json()
+            status = response_json["message"]
+            return status
+        return None
+
+    async def uninstall_game(self,game="arksa"):
+        print("Uninstalling game for " + game)
+        payload = {'game': game}
+        response = requests.delete(f'https://api.nitrado.net/services/{self.SERVER_ID}/gameservers/games/uninstall',
+                                 auth=BearerAuth(self.NITRADO_TOKEN), params=payload)
+        print("Status Code: " + str(response.status_code))
+        print("Response: " + str(response.content))
+        if response.status_code == 200:
+            response_json = response.json()
+            status = response_json["message"]
+            return status
+        return None
+
+    async def restart_server(self, message="Restarting...", restart_message="Restarting..."):
+        print("Restarting Server")
+        payload = {'message': message,"restart_message": restart_message}
+        response = requests.post(f'https://api.nitrado.net/services/{self.SERVER_ID}/gameservers/restart',
+                                 auth=BearerAuth(self.NITRADO_TOKEN), params=payload)
+        print("Status Code: " + str(response.status_code))
+        print("Response: " + str(response.content))
+        if response.status_code == 200:
+            response_json = response.json()
+            status = response_json["message"]
+            return status
+        return None
+    async def run_console_command(self, command=""):
+        print("Running command on Server: " + command)
+        with Client(self.RCON_IP, self.RCON_PORT, passwd=self.RCON_PWD) as client:
+            try:
+                response = client.run(command)
+            except Exception as e:
+                print(e)
+            else:
+                return response
+        return None
+
+class BearerAuth(requests.auth.AuthBase):
+    def __init__(self, token):
+        self.token = token
+
+    def __call__(self, r):
+        r.headers["authorization"] = "Bearer " + self.token
+        return r
